f60eff15b81a56ffff89c9a12b7f13dcca96a840
Roel-compsci <rrtorres38@gmail.com>
2025-04-09 19:12:00 -0400

    test fix

--- test/controllers/listings_controller_test.rb
+++ test/controllers/listings_controller_test.rb
@@ -1,6 +1,15 @@
 require "test_helper"
 
 class ListingsControllerTest < ActionDispatch::IntegrationTest
+  def sign_in_as(user)
+    post login_url, params: { username: user.username, password: 'password' }
+  end
+  
+  setup do
+    @user = User.create(username: 'testuser', password: 'password')
+    sign_in_as(@user)
+  end
+  
   test "should get index" do
     get listings_url
     assert_response :success
