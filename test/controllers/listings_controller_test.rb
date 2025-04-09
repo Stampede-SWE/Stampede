c288b06d1a91543a94baf063e3e9f168b711f639
Roel-compsci <rrtorres38@gmail.com>
2025-04-09 19:15:49 -0400

    lint fix 2

--- test/controllers/listings_controller_test.rb
+++ test/controllers/listings_controller_test.rb
@@ -4,12 +4,10 @@ class ListingsControllerTest < ActionDispatch::IntegrationTest
   def sign_in_as(user)
     post login_url, params: { username: user.username, password: "password" }
   end
-
   setup do
     @user = User.create(username: "testuser", password: "password")
     sign_in_as(@user)
   end
-  
   test "should get index" do
     get listings_url
     assert_response :success
