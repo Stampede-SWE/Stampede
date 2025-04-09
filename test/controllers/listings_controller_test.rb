2e7f4340b43f70f377a38c0da2266567a728d6d0
Roel-compsci <rrtorres38@gmail.com>
2025-04-09 19:13:05 -0400

    lint fix

--- test/controllers/listings_controller_test.rb
+++ test/controllers/listings_controller_test.rb
@@ -2,11 +2,11 @@ require "test_helper"
 
 class ListingsControllerTest < ActionDispatch::IntegrationTest
   def sign_in_as(user)
-    post login_url, params: { username: user.username, password: 'password' }
+    post login_url, params: { username: user.username, password: "password" }
   end
-  
+
   setup do
-    @user = User.create(username: 'testuser', password: 'password')
+    @user = User.create(username: "testuser", password: "password")
     sign_in_as(@user)
   end
   
