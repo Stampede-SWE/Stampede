ea5af2b2cef1f6622b9abdca9888a1695e4afea6
imnatewest <nathanwest@usf.edu>
2025-04-27 19:18:02 -0400

    bada bing bada boom Mr worldwide as i step in da room

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[8.0].define(version: 2025_04_27_004136) do
   create_table "listings", force: :cascade do |t|
     t.string "title"
